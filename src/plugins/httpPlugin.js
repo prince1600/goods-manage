feb73252c3af0ec16dd5d1a16f5ff867342b3b16
甘巧书 <ganqiaoshu@ganqiaoshudeMacBook-Air.local>
2021-02-27 15:21:15 +0800

    修改axios baseUrl

--- src/plugins/httpPlugin.js
+++ src/plugins/httpPlugin.js
@@ -4,7 +4,7 @@ const http = {}
 http.install = function (Vue, options) {
 
     // 添加实例方法
-    axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+    axios.defaults.baseURL = 'http://192.168.199.100:8888/api/private/v1/'
     Vue.prototype.$http = axios
 }
 
